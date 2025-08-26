ea57faf46baf48cd54d9d5b847b4480a0e48647c
Debashis Saha <debashis.saha@brainiuminfotech.com>
2025-08-27 00:25:33 +0530

    feat: remove obsolete macOS and Linux build configurations and scripts

--- zcc.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set MACOS_SDK=C:\Users\debas\Downloads\sdk-macos-12.0-main\sdk\root
-zig cc --sysroot %MACOS_SDK% -L%MACOS_SDK%\usr\lib -I%MACOS_SDK%\usr\include -F%MACOS_SDK%\System\Library\Frameworks -framework CoreFoundation -framework Security -target aarch64-macos-none %*
